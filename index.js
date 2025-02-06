c8b2352ecb67b9c47af65791c4cae36969097633
svrio0o <svkvr26@gmail.com>
2025-02-07 03:31:10 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.href = "home.html"
+}
+
+function about() {
+    window.location.href = "about.html"
+}
+
+function members() {
+    window.location.href = "members.html"
+}
