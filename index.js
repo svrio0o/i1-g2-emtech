d435bd97c2a2b94980a90d0b9871eb1e690a04bf
svrio0o <svkvr26@gmail.com>
2025-02-08 04:40:13 +0800

    update

--- index.js
+++ index.js
@@ -10,6 +10,47 @@ function members() {
     window.location.href = "members.html"
 }
 
+function diaz() {
+    window.location.href = "members/diaz.html"
+}
+
+function magtagad() {
+    window.location.href = "members/magtagad.html"
+}
+
+function asiwagan() {
+    window.location.href = "members/asiwagan.html"
+}
+
+function cruz() {
+    window.location.href = "members/cruz.html"
+}
+
+function jacobe() {
+    window.location.href = "members/jacobe.html"
+}
+
 function soto() {
     window.location.href = "members/soto.html"
+}
+
+function deGuzman() {
+    window.location.href = "members/deGuzman.html"
+}
+
+function campos() {
+    window.location.href = "members/campos.html"
+}
+
+function agriam() {
+    window.location.href = "members/agriam.html"
+}
+
+
+function casuga() {
+    window.location.href = "members/casuga.html"
+}
+
+function rioveros() {
+    window.location.href = "members/rioveros.html"
 }
